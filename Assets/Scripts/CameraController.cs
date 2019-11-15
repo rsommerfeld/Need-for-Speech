dc653541e155f1b17b3f58e98006b689a74ecb83
Franz Sauerwald <franz.sauerwald@live.de>
2019-11-15 15:26:06 +0100

    Add Car, CarController, simple Floor

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public GameObject objectToFollow;
+    public float offsetX;
+    public float offsetY;
+    public float offsetZ;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
