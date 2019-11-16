4adecc04bf06dc536a6a9c8a5c7553b9aba1500f
Benedikt Weber <benedikt.web23@gmail.com>
2019-11-16 17:32:03 +0100

    hard  commit

--- Assets/Scripts/CarButtonListener.cs
+++ Assets/Scripts/CarButtonListener.cs
@@ -9,17 +9,31 @@ public class CarButtonListener : MonoBehaviour {
     public Vector3 position = new Vector3(0,0,0);
     public Vector3 rotation = new Vector3(0,0,0);
     public Vector3 scale = new Vector3(1,1,1);
+    public Transform btn2, btn3;
+    public GameObject menuControler;
 
     void OnMouseUp()
     {
-        foreach (Transform child in car.transform)
+        Press();
+    }
+
+    public void Press()
+    {
+        if (menuControler.GetComponent<CameraButtonListener>().inMenu)
         {
-            GameObject.Destroy(child.gameObject);
+            foreach (Transform child in car.transform)
+            {
+                GameObject.Destroy(child.gameObject);
+            }
+            Component model = Instantiate(carmodel, position, Quaternion.identity);
+            model.transform.parent = car;
+            model.transform.localPosition = this.position;
+            model.transform.localScale = this.scale;
+            model.transform.localEulerAngles = this.rotation;
+
+            this.btn2.GetComponent<Renderer>().material.color = Color.white;
+            this.btn3.GetComponent<Renderer>().material.color = Color.white;
+            GetComponent<Renderer>().material.color = Color.blue;
         }
-        Component model = Instantiate(carmodel, position, Quaternion.identity);
-        model.transform.parent = car;
-        model.transform.localPosition = this.position;
-        model.transform.localScale = this.scale;
-        model.transform.localEulerAngles = this.rotation;
     }
 }
---
+++ Assets/Scripts/MenuVoiceController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuVoiceController : MonoBehaviour {
+
+    public GameObject audioController;
+    private AudioInput ai;
+
+    public Transform btn1, btn2, btn3;
+
+    // Use this for initialization
+    void Start ()
+    {
+        this.ai = audioController.GetComponent(typeof(AudioInput)) as AudioInput;
+        Debug.Log(ai);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        int c = gameObject.GetComponentsInChildren<Transform>().Length-2;
+        //Debug.Log("c "+c);
+        float dist = 3f / c;
+        //Debug.Log("dist " + dist);
+        int val = (int)( (ai.volume+1)/dist );
+        switch (val)
+        {
+            case 0:
+                btn1.GetComponentInChildren<CarButtonListener>().Press();
+                break;
+            case 1:
+                btn2.GetComponentInChildren<CarButtonListener>().Press();
+                break;
+            case 2:
+                btn3.GetComponentInChildren<CarButtonListener>().Press();
+                break;
+        }
+        //gameObject.GetComponentsInChildren<Transform>()[val].GetComponentInChildren<CarButtonListener>().Press();
+    }
+}
