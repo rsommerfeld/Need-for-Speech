daa9f405f9fb3412bb522f7870e42c0714c8412f
Franz Sauerwald <franz.sauerwald@live.de>
2019-11-16 16:54:36 +0100

    Add Scene Controller

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour {
+	// Use this for initialization
+	public void loadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
