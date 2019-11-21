90f84793243ec19777256fb215697df4ecb37449
rsommerfeld <55324905+rsommerfeld@users.noreply.github.com>
2019-11-21 10:17:31 +0100

    Drastically improved the accuracy of AudioInput. Most importantly, the variables minFreq and maxFreq are now specified in Hz and the Volume is specified in values between -90 and 0. Both are then processed to values between -1 and 1.

--- Assets/Scripts/AudioInput.cs
+++ Assets/Scripts/AudioInput.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 [RequireComponent(typeof(AudioSource))]
@@ -19,67 +20,44 @@ public class AudioInput : MonoBehaviour
     private Queue<float> volBuffer = new Queue<float>();
     private Queue<float> timeBuffer = new Queue<float>();
 
-    public float minfreq = -0.35f, avgfreq = 0.0f, maxfreq = 0.35f;
+    public const int minfreq = 100, maxfreq = 1100;
+    public int maxMeasuredFreq = 5120;
+    int maxI;
     //public float minfreq = -0.6f, avgfreq = -0.3f, maxfreq = 0f;
     //public float minfreq = -0.6f, avgfreq = 42f, maxfreq = -0.5f;
 
 
     //Script MicrophoneInput
-    void Start()
-    {
-        //Debug.Log("Start");
+    void Start(){
+
         aud = GetComponent<AudioSource>();
-        aud.clip = Microphone.Start(Microphone.devices[0], true, 1, 22050);
+        aud.clip = Microphone.Start(Microphone.devices[0], true, 1, maxMeasuredFreq);
         aud.loop = true;
-        //Debug.Log("Vor");
+
         while (!(Microphone.GetPosition(null) > 0)) { }
-        //Debug.Log("Nach");
         aud.Play();
 
-        //aud.Stop();
-
+        //initialize maxI (which is used for measuring the frequency)
+        maxI = 1 + ((maxMeasuredFreq * 512) / AudioSettings.outputSampleRate);
     }
+
     // Update is called once per frame
-    void Update()
-    {
-        //aud.clip = Microphone.Start(Microphone.devices[0], false, 1, 44100);
-        float[] data = new float[128];
-        float fundamentalFrequency = 0.0f;
+    void Update(){
+        
+        float frequency = getFrequency();
+        float volume = getVolume();
 
-        //Debug.Log(data);
-        aud.GetSpectrumData(data, 0, FFTWindow.BlackmanHarris);
+        float normalizedVolume = getNormalizedVolume(volume);
+        float normalizedFrequency = getNormalizedFrequency(frequency);
 
-        float s = 0.0f;
-        int i = 0;
-        float sum = 0;
-        for (int j = 0; j < 128; j++)
-        {
-            if (s < data[j])
-            {
-                s = data[j];
-                i = j;
-            }
 
-        }
-        fundamentalFrequency = i * 22100 / 128;
-        float Volume = 15 * LevelMax();
-        float zcord = (fundamentalFrequency - 500) / 500;
-        if (zcord > 1) zcord = 1;
 
-        if (oldFreq < zcord) oldFreq = oldFreq + Smoothness;
-        else oldFreq = oldFreq - Smoothness;
+        //Debug.Log("Freq: " + normalizedFrequency + " (" + frequency + ") Vol: " + normalizedVolume + " (" + volume + ")");
 
-        if (oldVolume < zcord) oldVolume = oldVolume + Smoothness;
-        else oldVolume = oldVolume - Smoothness;
-
-        var freq = oldFreq;
-        var vol = (2 * Volume - 10) / 10;
-        //this.frequency = oldFreq;
-        //this.volume = (2 * Volume - 10) / 10;
-
+        //TRANSFER
         float curTime = Time.time;
-        volBuffer.Enqueue(vol);
-        freqBuffer.Enqueue(freq);
+        volBuffer.Enqueue(normalizedVolume);
+        freqBuffer.Enqueue(normalizedFrequency);
         timeBuffer.Enqueue(curTime);
 
         float volsum = 0;
@@ -92,32 +70,13 @@ public class AudioInput : MonoBehaviour
         {
             freqsum += v;
         }
-        /*Debug.Log("cnt: " + volBuffer.Count);
-        Debug.Log("timeheaddelta: " + (curTime-timeBuffer.Peek()));*/
+
         this.volume = volsum / volBuffer.Count;
         this.frequency = (freqsum / freqBuffer.Count);
-        if (this.frequency > this.avgfreq)
-        {
-            this.frequency = 1 - Mathf.Max(0, this.maxfreq - this.frequency) / (this.maxfreq - this.avgfreq);
-        }
-        else
-        {
-            this.frequency = Mathf.Max(this.minfreq, -this.avgfreq + this.frequency) / (this.avgfreq - this.minfreq);
-        }
-        /*Debug.Log("origfreq "+this.frequency);
-        if(this.frequency < this.minfreq)
-        {
-            this.frequency = -1;
-        } else if (this.frequency > this.maxfreq)
-        {
-            this.frequency = 1;
-        } else
-        {
-            this.frequency = 0;
-        }*/
+        
+
         if (this.volume < 0)
         {
-            //this.volume = 
             this.frequency = 0;
         }
         while (timeBuffer.Count > 0 && (curTime - timeBuffer.Peek()) > 0.1)
@@ -126,27 +85,89 @@ public class AudioInput : MonoBehaviour
             volBuffer.Dequeue();
             freqBuffer.Dequeue();
         }
-
     }
 
-    float LevelMax()
-    {
+
+    //produces values between -100 and 100
+    float getNormalizedVolume(float volume){
+        if(volume < -90) volume = -90; //to rule out infinity
+
+        float percentage = (volume + 90) * 200;
+        percentage = percentage / 90;
+        percentage = percentage - 100; //percentage should be between -10 and 100 now
+
+        return percentage / 100;
+    }
+
+
+    //returns values between -90 and 0, while 0 is the maximum
+    float getVolume(){
+
         AudioClip _clipRecord = new AudioClip();
         _clipRecord = aud.clip;
+
         float LevelMax = 0;
+        float original = 0;
+
         float[] waveData = new float[128];
         int micPosition = Microphone.GetPosition(null) - (128 + 1);
         if (micPosition < 0) return 0;
+
         _clipRecord.GetData(waveData, micPosition);
-        for (int i = 0; i < 128; i++)
-        {
+
+        for (int i = 0; i < 128; i++) {
+
             float wavePeak = waveData[i] * waveData[i];
-            if (LevelMax < wavePeak)
-            {
+            if (LevelMax < wavePeak){
+
                 LevelMax = wavePeak;
+                original = waveData[i];
+
             }
         }
-        return Mathf.Sqrt(Mathf.Sqrt(LevelMax));
 
+            float db = 20 * Mathf.Log10(Mathf.Abs(original));
+            return db;
+    }
+
+
+    //produces values between -100 and 100
+    float getNormalizedFrequency(float frequency){
+
+        if(frequency > maxfreq) frequency = maxfreq;
+        if(frequency < minfreq) frequency = minfreq;
+
+        frequency = frequency - minfreq;
+        float difference = maxfreq - minfreq;
+
+        float percentage = 200 * frequency;
+        percentage = percentage / difference;
+        percentage = percentage - 100;
+
+        return percentage / 100;
+    }
+
+    //returns the single loudest frequency of the measured spectrum
+    float getFrequency(){
+
+        float[] data = new float[512];
+        float fundamentalFrequency = 0.0f;
+
+        //Debug.Log(data);
+        aud.GetSpectrumData(data, 0, FFTWindow.BlackmanHarris);
+
+        float s = 0.0f;
+        int i = 0;
+        float sum = 0;
+        for (int j = 0; j < maxI; j++)
+        {
+            if (s < data[j])
+            {
+                s = data[j];
+                i = j;
+            }
+        }
+        fundamentalFrequency = i * AudioSettings.outputSampleRate / 1024;
+        return fundamentalFrequency;
     }
 }
